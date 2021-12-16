c00a7d567ab0ab6bdb4d41cb28039da174e7aacf
Tanavjayadeesh <95433338+Tanavjayadeesh@users.noreply.github.com>
2021-12-16 20:55:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
-
 function preload(){
+  pathImage = loadImage("path.png");
+  
+  RunnerImage = loadImage("cloud.png");
 }
 
 function setup(){
   createCanvas(400,400);
-  Runner.createSprite = (400,400,50,50)
-  path.createSprite(200,200);
-  path.addImage(pathImg);
+  Runner = createSprite = (400,400,50,50)
+  path = createSprite = (200,200);
+  path.addImage(path.png);
   path.velocity =  4;
   path.scale= 1.2;
 }
 
-if(path.y > 400 ){
+if(path.y = 400 ) {
   path.y = height/2;
 }
 
